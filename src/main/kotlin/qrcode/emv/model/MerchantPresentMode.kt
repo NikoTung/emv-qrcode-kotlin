f0646ce8bc825debc7c7b7266cd08f90522b1951
niko <n@eiko.me>
2024-06-14 17:05:45 +0800

    docs

--- src/main/kotlin/qrcode/emv/model/MerchantPresentMode.kt
+++ src/main/kotlin/qrcode/emv/model/MerchantPresentMode.kt
@@ -5,97 +5,70 @@ import qrcode.emv.core.mpm.MerchantPresentModeCode
 import qrcode.emv.core.mpm.Tag
 import qrcode.emv.exception.InvalidTagException
 
-class MerchantPresentMode(val tags: MutableMap<String, Tag>) {
+class MerchantPresentMode(private val tags: MutableMap<String, Tag>) {
 
 
     fun add(tag: Tag) {
         tags[tag.tag] = tag
     }
 
-    fun get(id: String): Tag {
-        return tags[id] ?: throw InvalidTagException("Tag $id not found")
+    fun get(id: String): Tag? {
+        return tags[id]
     }
 
-    fun get(id: Int): Tag {
+    fun get(id: Int): Tag? {
         return get(String.format("%02d", id))
     }
 
 
-    fun getPayloadFormatIndicator(): Tag {
-        return get(MerchantPresentModeCode.PAYLOAD_FORMAT_INDICATOR.tag.first)
-    }
 
-    fun getPointOfInitiationMethod(): Tag {
-        return get(MerchantPresentModeCode.POINT_OF_INITIATION_METHOD.tag.first)
-    }
 
-    fun getMerchantAccountInformationReserved(): List<Tag> {
-        return MerchantPresentModeCode.MERCHANT_ACCOUNT_INFORMATION_RESERVED.tag.mapNotNull { get(it) }
-    }
+    fun getPayloadFormatIndicator(): Tag? = get(MerchantPresentModeCode.PAYLOAD_FORMAT_INDICATOR.tag.first)
 
-    fun getMerchantAccountInformation(): List<Tag> {
-        return MerchantPresentModeCode.MERCHANT_ACCOUNT_INFORMATION_TEMPLATE.tag.mapNotNull { get(it) }
-    }
+    fun getPointOfInitiationMethod(): Tag? = get(MerchantPresentModeCode.POINT_OF_INITIATION_METHOD.tag.first)
 
-    fun getMerchantCategoryCode(): Tag {
-        return get(MerchantPresentModeCode.MERCHANT_CATEGORY_CODE.tag.first)
-    }
+    fun getMerchantAccountInformationReserved(): List<Tag> =
+        MerchantPresentModeCode.MERCHANT_ACCOUNT_INFORMATION_RESERVED.tag.mapNotNull { get(it) }
 
-    fun getTransactionCurrency(): Tag {
-        return get(MerchantPresentModeCode.TRANSACTION_CURRENCY.tag.first)
-    }
 
-    fun getTransactionAmount(): Tag {
-        return get(MerchantPresentModeCode.TRANSACTION_AMOUNT.tag.first)
-    }
+    fun getMerchantAccountInformation(): List<Tag> =
+        MerchantPresentModeCode.MERCHANT_ACCOUNT_INFORMATION_TEMPLATE.tag.mapNotNull { get(it) }
 
-    fun getTipOrConvenienceIndicator(): Tag {
-        return get(MerchantPresentModeCode.TIP_OR_CONVENIENCE_INDICATOR.tag.first)
-    }
 
-    fun getValueOfConvenienceFeeFixed(): Tag {
-        return get(MerchantPresentModeCode.VALUE_OF_CONVENIENCE_FEE_FIXED.tag.first)
-    }
+    fun getMerchantCategoryCode(): Tag? = get(MerchantPresentModeCode.MERCHANT_CATEGORY_CODE.tag.first)
 
-    fun getValueOfConvenienceFeePercentage(): Tag {
-        return get(MerchantPresentModeCode.VALUE_OF_CONVENIENCE_FEE_PERCENTAGE.tag.first)
-    }
+    fun getTransactionCurrency(): Tag? = get(MerchantPresentModeCode.TRANSACTION_CURRENCY.tag.first)
 
-    fun getCountryCode(): Tag {
-        return get(MerchantPresentModeCode.COUNTRY_CODE.tag.first)
-    }
+    fun getTransactionAmount(): Tag? = get(MerchantPresentModeCode.TRANSACTION_AMOUNT.tag.first)
 
-    fun getMerchantName(): Tag {
-        return get(MerchantPresentModeCode.MERCHANT_NAME.tag.first)
-    }
+    fun getTipOrConvenienceIndicator(): Tag? = get(MerchantPresentModeCode.TIP_OR_CONVENIENCE_INDICATOR.tag.first)
 
-    fun getMerchantCity(): Tag {
-        return get(MerchantPresentModeCode.MERCHANT_CITY.tag.first)
-    }
 
-    fun getPostalCode(): Tag {
-        return get(MerchantPresentModeCode.POSTAL_CODE.tag.first)
-    }
+    fun getValueOfConvenienceFeeFixed(): Tag? = get(MerchantPresentModeCode.VALUE_OF_CONVENIENCE_FEE_FIXED.tag.first)
 
-    fun getAdditionalDataFieldTemplate(): Tag {
-        return get(MerchantPresentModeCode.ADDITIONAL_DATA_FIELD_TEMPLATE.tag.first)
-    }
+    fun getValueOfConvenienceFeePercentage(): Tag? =
+        get(MerchantPresentModeCode.VALUE_OF_CONVENIENCE_FEE_PERCENTAGE.tag.first)
 
-    fun getCrc(): Tag {
-        return get(MerchantPresentModeCode.CRC.tag.first)
-    }
+    fun getCountryCode(): Tag? = get(MerchantPresentModeCode.COUNTRY_CODE.tag.first)
 
-    fun getMerchantInformationLanguageTemplate(): Tag {
-        return get(MerchantPresentModeCode.MERCHANT_INFORMATION_LANGUAGE_TEMPLATE.tag.first)
-    }
+    fun getMerchantName(): Tag? = get(MerchantPresentModeCode.MERCHANT_NAME.tag.first)
 
-    fun getRfuForEmvco(): List<Tag> {
-        return MerchantPresentModeCode.RFU_FOR_EMVCO.tag.mapNotNull { get(it) }
-    }
+    fun getMerchantCity(): Tag? = get(MerchantPresentModeCode.MERCHANT_CITY.tag.first)
 
-    fun getUnreservedTemplates(): List<Tag> {
-        return MerchantPresentModeCode.UNRESERVED_TEMPLATES.tag.mapNotNull { get(it) }
-    }
+    fun getPostalCode(): Tag? = get(MerchantPresentModeCode.POSTAL_CODE.tag.first)
+
+    fun getAdditionalDataFieldTemplate(): Tag? = get(MerchantPresentModeCode.ADDITIONAL_DATA_FIELD_TEMPLATE.tag.first)
+
+    fun getCrc(): Tag? = get(MerchantPresentModeCode.CRC.tag.first)
+
+
+    fun getMerchantInformationLanguageTemplate(): Tag? =
+        get(MerchantPresentModeCode.MERCHANT_INFORMATION_LANGUAGE_TEMPLATE.tag.first)
+
+    fun getRfuForEmvco(): List<Tag> = MerchantPresentModeCode.RFU_FOR_EMVCO.tag.mapNotNull { get(it) }
+
+
+    fun getUnreservedTemplates(): List<Tag> = MerchantPresentModeCode.UNRESERVED_TEMPLATES.tag.mapNotNull { get(it) }
 
 
     override fun toString(): String {
